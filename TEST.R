e13764918c75ae10d261fee198174b0495d97a52
tomass22 <dominanthat@gmail.com>
2022-05-03 13:54:31 +0900

    Add commit tool download

--- TEST.R
+++ TEST.R
@@ -1,32 +1,63 @@
-a <- 10
-View(a)
-b <- c(1,2,3,4,5,6,7,8,9,0)
-View(b)
-b + a
+
 .libPaths()
 .libPaths()
+
 #[1] "C:/Program Files/R/R-4.0.2/library"
 #[출처] [R] 패키지 저장 경로 변경하기|작성자 리얼
 
-
-#install.packages(c('R6', 'jsonlite'))
+##<languageserver 패키지 설치>
 #Sys.getenv('R_LIBS_USER')
 #fLib <- Sys.getenv('R_LIBS_USER')
 #fLib
 #fLibScales <- paste0(fLib, '/languageserver')
 #fLibScales
 #unlink(fLibScales, recursive=TRUE)
-Sys.getenv('R_LIBS_USER')
-fLib <- Sys.getenv('R_LIBS_USER')
-fLib
-fLibScales <- paste0(fLib, '/languageserver')
-fLibScales
-unlink(fLibScales, recursive=TRUE)
-install.packages("languageserver")
-install.packages('languageserver', "C:\\RLibs", 'http://cran.stat.sfu.ca/')
+#install.packages("languageserver")
+#install.packages('languageserver', "C:\\RLibs", 'http://cran.stat.sfu.ca/')
 #출처: https://sealover.tistory.com/209 [바닷가에서 놀자!!]
+#install.packages("languageserver", INSTALL_opts = '--no-lock')
 
-install.packages("languageserver", INSTALL_opts = '--no-lock')
+#getwd()
+#setwd("C:/프로그래밍/vscode_workstation/R_workstation")
 
-getwd()
-setwd("C:/프로그래밍/vscode_workstation/R_workstation")
+
+##<httpgd 패키지 설치>
+#fLib <- Sys.getenv('R_LIBS_USER')
+#fLib
+#fLibScales <- paste0(fLib, '/httpdg')
+#fLibScales
+#unlink(fLibScales, recursive=TRUE)
+#install.packages( " httpgd " )
+remotes::install_github("nx10/httpgd")
+
+
+##Rtools 설치
+# writeLines('PATH="${RTOOLS40_HOME}\\usr\\bin;${PATH}"', con = "~/.Renviron")
+
+# Sys.which("make")
+
+# Sys.getenv('R_LIBS_USER')
+# fLib <- Sys.getenv('R_LIBS_USER')
+# fLib
+# fLibScales <- paste0(fLib, '/jsonlite')
+# fLibScales
+# unlink(fLibScales, recursive=TRUE)
+# install.packages("jsonlite", type = "source")
+
+##radian 설치
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- TestDebugger.R
+++ TestDebugger.R
@@ -2,9 +2,11 @@ getwd()
 a <- 10
 b <- 20
 c <- c(10,20,30)
+
 print(a)
 print(b)
 print(c)
 View(a)
 rm(a,b,c)
+
 ## succesfull debuging
